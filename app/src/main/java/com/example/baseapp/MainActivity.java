d1753409cd7223efec4a9058573f7e0b44f9f2f4
yooonji08 <67667665+yooonji08@users.noreply.github.com>
2021-04-16 21:56:04 +0900

    baseapp_error commit

---
+++ app/src/main/java/com/example/baseapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.baseapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static android.widget.Toast.*;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.R100);
+        //button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
